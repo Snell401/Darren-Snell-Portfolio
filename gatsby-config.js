21388c11e92134c58b6b32a1abebd3b8e36eeb03
Darren Snell <darren_snell@hotmail.co.uk>
2022-04-22 10:50:06 +0200

    changed some background

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
       options: {
-        
+        showFooter: false,
       },
     },
     {
